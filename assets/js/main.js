5319c15c70bd5bbdaed6298e662a055a2cb40605
Sajal Fatima <sf08108@st.habib.edu.pk>
2025-02-21 16:29:02 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// main.js - Initialize any JavaScript functionality here
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Fashion Hub website loaded successfully.");
+  });
+  
